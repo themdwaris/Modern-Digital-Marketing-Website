4add2833694350262161cc8a5c5b1d01f1d2545a
Mohammad Waris <mdtech9900@gmail.com>
2022-09-04 12:56:40 +0530

    Pusing Digital marketing website

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+function openMenu(){
+    document.getElementById("openMenu").style.left = "0px";
+    
+}
+function closeMenu(){
+    document.getElementById("openMenu").style.left = "-280px";
+    
+}
